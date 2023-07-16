fd1a969809c94db8f2d95dc916b2e30f9a6ea293
Luis Riegger <lri@me.com>
2023-07-16 11:11:56 +0200

    add linear test

---
+++ test/test_linear.py
@@ -0,0 +1,10 @@
+import numpy as np
+from sensitivity.linear import propagate_errors
+
+def my_function(x, y):
+    return [x ** 2, y]
+
+def test_propagate_errors():
+    result = propagate_errors(my_function, errors=[0.1, 0.2], x0=[1, 1])
+    expected = np.array([0.2, 0.2])
+    assert np.allclose(result, expected)
