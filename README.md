ce3c9c901898f8c2df5ca7b48567c696fb252d9a
Luis Riegger <lri@me.com>
2023-07-16 10:34:56 +0200

    update linear and readme

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Sensitivity
 
+Methods for linear and stochastic sensitivity analysis and error propagation.
+
 ## Install
 
 You can install the `sensitivity` module using pip:
@@ -12,9 +14,42 @@ pip install sensitivity
 ## Usage
 
 
-### sensitivity.stochastic
+### `sensitivity.stochastic`
 
 The `sensitivity.stochastic` module provides functionality for stochastic sensitivity analysis. It allows you to stochastically sample input parameters from various distributions and evaluate the sensitivity of a function to those inputs.
 
+Example for stochastic sampling:
 
-### sensitivity.linear
+```python
+from sensitivity.stochastic import sample, Gaussian, Uniform
+
+def my_function(x, y):
+    return x + y
+
+samples = sample(my_function, [Gaussian(0, 0.03), Uniform(-1, 1)], n=100)
+
+# Perform sensitivity analysis on the samples
+# ...
+```
+
+
+### `sensitivity.linear`
+
+This module contains the `propagate_errors` function for linear propagation of errors for a given function based on the principle of differentials. It makes use of the scipy function `approx_fprime` to calculate the partial derivatives of the given function. The propagated error is calculated by taking the square root of the sum of squares of the product of the partial derivatives and the uncertainties.
+
+Here is a simple usage example:
+
+```python
+from sensitivity.linear import propagate_errors
+
+def my_function(x, y):
+    return [x ** 2, y]
+
+print(propagate_errors(my_function, errors=[0.1, 0.2], x0=[1, 1]))
+```
+
+This will output:
+
+```python
+np.array([0.2, 0.2])
+```
--- src/sensitivity/linear.py
+++ src/sensitivity/linear.py
@@ -9,7 +9,41 @@ def propagate_errors(
     errors: npt.ArrayLike,
     x0: npt.ArrayLike,
     epsilon: float | npt.ArrayLike = _epsilon,
-):
+) -> np.ndarray:
+
+    """
+    Propagate errors of function input arguments 
+    based on differentials around central values.
+
+    It uses the scipy function `approx_fprime` to calculate the 
+    partial derivatives of f. The propagated error is calculated 
+    by taking the square root of the sum of squares of the product 
+    of the partials and the uncertainties.
+
+    Args:
+        f (function): The function for which the propagation of errors 
+            is to be calculated.
+        errors: Uncertainties in the arguments of f. 
+            Each uncertainty corresponds to a single argument of f.
+        x0: Initial values for the arguments of f. 
+            The function f will be evaluated around these points.
+        epsilon (optional): Step sizes for the central difference approximation
+            of the gradient. Defaults to sqrt(machine epsilon) 
+            for float data type. If it is an array, 
+            it should have the same shape as x0.
+
+    Returns:
+        Propagated errors. Each element is the propagated error for a parameter of f 
+                    evaluated at the corresponding value in x0.
+
+    Example:
+        >>> def my_function(x, y):
+        ...     return [x ** 2, y]
+
+        >>> propagate_errors(my_function, errors=[0.1, 0.2], x0=[1, 1])
+        np.array([0.2, 0.2])
+    """
+
     # Wrapper function to allow f to accept a single list of parameters
     def wrapper(x):
         return f(*x)
@@ -18,6 +52,7 @@ def propagate_errors(
     partials = approx_fprime(x0, wrapper, epsilon)
 
     # Calculate error in function output
-    propagated_errors = np.sqrt(np.sum(np.square(partials * errors)))
+    errors = np.array(errors)
+    propagated_errors = np.sqrt(np.sum(np.square(partials * errors), axis=-1))
 
     return propagated_errors
